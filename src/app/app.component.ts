e12c71438abc125f4683a126a1ecacf9454c4a6a
beatmercy <a304921113@live.com>
2018-08-08 09:03:03 +0800

    angular animation try

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
 import { ThemePalette } from '@angular/material';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('titleState', [
+      state('inactive', style({
+        backgroundColor: '#eee',
+        transform: 'scale(1)'
+      })),
+      state('active', style({
+        backgroundColor: '#cfd8dc',
+        transform: 'scale(1.1)'
+      })),
+      transition('inactive => active', animate('100ms ease-in')),
+      transition('active => inactive', animate('100ms ease-out'))
+    ])]
 })
 export class AppComponent {
   title = 'app';
   color: ThemePalette;
+  titleState = 'inactive';
+
+  toggleTitleState() {
+    this.titleState = this.titleState === 'inactive' ? this.titleState = 'active' : 'inactive';
+  }
 }
