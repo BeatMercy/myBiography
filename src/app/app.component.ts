0868c353551aedf3cfc438e681c8aea954f8502b
beatmercy <a304921113@live.com>
2018-09-09 01:14:00 +0800

    scss support masonry layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { ThemePalette } from '@angular/material';
 import { trigger, state, style, transition, animate } from '@angular/animations';
 import * as showdown from 'showdown';
+import { MASONRIES } from './mock-datas/masonry';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.scss'],
   animations: [
     trigger('titleState', [
       state('inactive', style({
@@ -17,21 +18,26 @@ import * as showdown from 'showdown';
         backgroundColor: '#cfd8dc',
         transform: 'scale(1.1)'
       })),
-      transition('inactive => active', animate('100ms ease-in')),
+      transition('* => void', animate('100ms ease-in')),
       transition('active => inactive', animate('100ms ease-out'))
     ])]
 })
+/**
+ * 博客主界面
+ * Blog main page
+ */
 export class AppComponent implements OnInit {
   title = 'app';
   color: ThemePalette;
   titleState = 'inactive';
-  blogText = '';
+
+  masonries = MASONRIES;
 
   ngOnInit(): void {
-    const converter = new showdown.Converter();
-    this.blogText = converter.makeHtml('#hellow Markdown');
+    console.log('something out?!');
+
   }
   toggleTitleState() {
-    this.titleState = this.titleState === 'inactive' ? this.titleState = 'active' : 'inactive';
+    this.titleState === 'active' ? this.titleState = null : this.titleState = 'active';
   }
 }
---
+++ src/app/mock-datas/masonry.ts
@@ -0,0 +1,391 @@
+export const MASONRIES = [{
+    title: 'DEMO',
+    subtitle: 'this is a subtitle',
+    tags: [{
+        name: 'TECH',
+        value: 'tech'
+    }, {
+        name: 'READING',
+        value: 'reading'
+    }],
+    abstract: 'TEROR is very danger with nuclear here. For the security',
+    images: [{
+        caption: 'image here',
+        path: 'assets/images/umaru.jpg',
+    }]
+}, {
+    title: 'DEMO',
+    subtitle: 'this is a subtitle',
+    tags: [{
+        name: 'TECH',
+        value: 'tech'
+    }, {
+        name: 'READING',
+        value: 'reading'
+    }],
+    abstract: 'TEROR is very danger with nuclear here. For the security',
+    images: [{
+        caption: 'image here',
+        path: 'assets/images/n15.jpg',
+    }]
+}, {
+    title: 'DEMO',
+    subtitle: 'this is a subtitle',
+    tags: [{
+        name: 'TECH',
+        value: 'tech'
+    }, {
+        name: 'READING',
+        value: 'reading'
+    }],
+    abstract: 'TEROR is very danger with nuclear here. For the security',
+    images: [{
+        caption: 'image here',
+        path: 'assets/images/umaru.jpg',
+    }]
+}, {
+    title: 'DEMO',
+    subtitle: 'this is a subtitle',
+    tags: [{
+        name: 'TECH',
+        value: 'tech'
+    }, {
+        name: 'READING',
+        value: 'reading'
+    }],
+    abstract: 'TEROR is very danger with nuclear here. For the security',
+    images: [{
+        caption: 'image here',
+        path: 'assets/images/umaru.jpg',
+    }]
+}, {
+    title: 'DEMO',
+    subtitle: 'this is a subtitle',
+    tags: [{
+        name: 'TECH',
+        value: 'tech'
+    }, {
+        name: 'READING',
+        value: 'reading'
+    }],
+    abstract: 'TEROR is very danger with nuclear here. For the security',
+    images: [{
+        caption: 'image here',
+        path: 'assets/images/umaru.jpg',
+    }]
+}, {
+    title: 'DEMO',
+    subtitle: 'this is a subtitle',
+    tags: [{
+        name: 'TECH',
+        value: 'tech'
+    }, {
+        name: 'READING',
+        value: 'reading'
+    }],
+    abstract: 'TEROR is very danger with nuclear here. For the security',
+    images: [{
+        caption: 'image here',
+        path: 'assets/images/15.jpg',
+    }]
+}, {
+    title: 'DEMO',
+    subtitle: 'this is a subtitle',
+    tags: [{
+        name: 'TECH',
+        value: 'tech'
+    }, {
+        name: 'READING',
+        value: 'reading'
+    }],
+    abstract: 'TEROR is very danger with nuclear here. For the security',
+    images: [{
+        caption: 'image here',
+        path: 'assets/images/toms.png',
+    }]
+}, {
+    title: 'DEMO',
+    subtitle: 'this is a subtitle',
+    tags: [{
+        name: 'TECH',
+        value: 'tech'
+    }, {
+        name: 'READING',
+        value: 'reading'
+    }],
+    abstract: 'TEROR is very danger with nuclear here. For the security',
+    images: [{
+        caption: 'image here',
+        path: 'assets/images/toms.png',
+    }]
+}, {
+    title: 'DEMO',
+    subtitle: 'this is a subtitle',
+    tags: [{
+        name: 'TECH',
+        value: 'tech'
+    }, {
+        name: 'READING',
+        value: 'reading'
+    }],
+    abstract: 'TEROR is very danger with nuclear here. For the security',
+    images: [{
+        caption: 'image here',
+        path: 'assets/images/umaru.jpg',
+    }]
+}, {
+    title: 'DEMO',
+    subtitle: 'this is a subtitle',
+    tags: [{
+        name: 'TECH',
+        value: 'tech'
+    }, {
+        name: 'READING',
+        value: 'reading'
+    }],
+    abstract: 'TEROR is very danger with nuclear here. For the security',
+    images: [{
+        caption: 'image here',
+        path: 'assets/images/toms.png',
+    }]
+}, {
+    title: 'DEMO',
+    subtitle: 'this is a subtitle',
+    tags: [{
+        name: 'TECH',
+        value: 'tech'
+    }, {
+        name: 'READING',
+        value: 'reading'
+    }],
+    abstract: 'TEROR is very danger with nuclear here. For the security',
+    images: [{
+        caption: 'image here',
+        path: 'assets/images/umaru.jpg',
+    }]
+}, {
+    title: 'DEMO',
+    subtitle: 'this is a subtitle',
+    tags: [{
+        name: 'TECH',
+        value: 'tech'
+    }, {
+        name: 'READING',
+        value: 'reading'
+    }],
+    abstract: 'TEROR is very danger with nuclear here. For the security',
+    images: [{
+        caption: 'image here',
+        path: 'assets/images/15.jpg',
+    }]
+}, {
+    title: 'DEMO',
+    subtitle: 'this is a subtitle',
+    tags: [{
+        name: 'TECH',
+        value: 'tech'
+    }, {
+        name: 'READING',
+        value: 'reading'
+    }],
+    abstract: 'TEROR is very danger with nuclear here. For the security',
+    images: [{
+        caption: 'image here',
+        path: 'assets/images/umaru.jpg',
+    }]
+}, {
+    title: 'DEMO',
+    subtitle: 'this is a subtitle',
+    tags: [{
+        name: 'TECH',
+        value: 'tech'
+    }, {
+        name: 'READING',
+        value: 'reading'
+    }],
+    abstract: 'TEROR is very danger with nuclear here. For the security',
+    images: [{
+        caption: 'image here',
+        path: 'assets/images/n15.jpg',
+    }]
+}, {
+    title: 'DEMO',
+    subtitle: 'this is a subtitle',
+    tags: [{
+        name: 'TECH',
+        value: 'tech'
+    }, {
+        name: 'READING',
+        value: 'reading'
+    }],
+    abstract: 'TEROR is very danger with nuclear here. For the security',
+    images: [{
+        caption: 'image here',
+        path: 'assets/images/umaru.jpg',
+    }]
+}, {
+    title: 'DEMO',
+    subtitle: 'this is a subtitle',
+    tags: [{
+        name: 'TECH',
+        value: 'tech'
+    }, {
+        name: 'READING',
+        value: 'reading'
+    }],
+    abstract: 'TEROR is very danger with nuclear here. For the security',
+    images: [{
+        caption: 'image here',
+        path: 'assets/images/umaru.jpg',
+    }]
+}, {
+    title: 'DEMO',
+    subtitle: 'this is a subtitle',
+    tags: [{
+        name: 'TECH',
+        value: 'tech'
+    }, {
+        name: 'READING',
+        value: 'reading'
+    }],
+    abstract: 'TEROR is very danger with nuclear here. For the security',
+    images: [{
+        caption: 'image here',
+        path: 'assets/images/umaru.jpg',
+    }]
+}, {
+    title: 'DEMO',
+    subtitle: 'this is a subtitle',
+    tags: [{
+        name: 'TECH',
+        value: 'tech'
+    }, {
+        name: 'READING',
+        value: 'reading'
+    }],
+    abstract: 'TEROR is very danger with nuclear here. For the security',
+    images: [{
+        caption: 'image here',
+        path: 'assets/images/umaru.jpg',
+    }]
+}, {
+    title: 'DEMO',
+    subtitle: 'this is a subtitle',
+    tags: [{
+        name: 'TECH',
+        value: 'tech'
+    }, {
+        name: 'READING',
+        value: 'reading'
+    }],
+    abstract: 'TEROR is very danger with nuclear here. For the security',
+    images: [{
+        caption: 'image here',
+        path: 'assets/images/umaru.jpg',
+    }]
+}, {
+    title: 'DEMO',
+    subtitle: 'this is a subtitle',
+    tags: [{
+        name: 'TECH',
+        value: 'tech'
+    }, {
+        name: 'READING',
+        value: 'reading'
+    }],
+    abstract: 'TEROR is very danger with nuclear here. For the security',
+    images: [{
+        caption: 'image here',
+        path: 'assets/images/umaru.jpg',
+    }]
+}, {
+    title: 'DEMO',
+    subtitle: 'this is a subtitle',
+    tags: [{
+        name: 'TECH',
+        value: 'tech'
+    }, {
+        name: 'READING',
+        value: 'reading'
+    }],
+    abstract: 'TEROR is very danger with nuclear here. For the security',
+    images: [{
+        caption: 'image here',
+        path: 'assets/images/umaru.jpg',
+    }]
+}, {
+    title: 'DEMO',
+    subtitle: 'this is a subtitle',
+    tags: [{
+        name: 'TECH',
+        value: 'tech'
+    }, {
+        name: 'READING',
+        value: 'reading'
+    }],
+    abstract: 'TEROR is very danger with nuclear here. For the security',
+    images: [{
+        caption: 'image here',
+        path: 'assets/images/umaru.jpg',
+    }]
+}, {
+    title: 'DEMO',
+    subtitle: 'this is a subtitle',
+    tags: [{
+        name: 'TECH',
+        value: 'tech'
+    }, {
+        name: 'READING',
+        value: 'reading'
+    }],
+    abstract: 'TEROR is very danger with nuclear here. For the security',
+    images: [{
+        caption: 'image here',
+        path: 'assets/images/umaru.jpg',
+    }]
+}, {
+    title: 'DEMO',
+    subtitle: 'this is a subtitle',
+    tags: [{
+        name: 'TECH',
+        value: 'tech'
+    }, {
+        name: 'READING',
+        value: 'reading'
+    }],
+    abstract: 'TEROR is very danger with nuclear here. For the security',
+    images: [{
+        caption: 'image here',
+        path: 'assets/images/umaru.jpg',
+    }]
+}, {
+    title: 'DEMO',
+    subtitle: 'this is a subtitle',
+    tags: [{
+        name: 'TECH',
+        value: 'tech'
+    }, {
+        name: 'READING',
+        value: 'reading'
+    }],
+    abstract: 'TEROR is very danger with nuclear here. For the security',
+    images: [{
+        caption: 'image here',
+        path: 'assets/images/umaru.jpg',
+    }]
+}, {
+    title: 'DEMO',
+    subtitle: 'this is a subtitle',
+    tags: [{
+        name: 'TECH',
+        value: 'tech'
+    }, {
+        name: 'READING',
+        value: 'reading'
+    }],
+    abstract: 'TEROR is very danger with nuclear here. For the security',
+    images: [{
+        caption: 'image here',
+        path: 'assets/images/umaru.jpg',
+    }]
+}];
