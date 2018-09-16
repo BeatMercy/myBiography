e44f8dbb3a9b490c35d415657d9725015aa66497
beatmercy <a304921113@live.com>
2018-09-17 00:45:26 +0800

    side nav update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { ThemePalette } from '@angular/material';
 import { trigger, state, style, transition, animate } from '@angular/animations';
+import { of, BehaviorSubject, Observable } from 'rxjs';
+import { debounceTime, merge, delay } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   animations: [
-    trigger('titleState', [
+    trigger('sideExpand', [
       state('inactive', style({
-        background: 'white'
+        width: '15%'
       })),
       state('active', style({
-        background: 'pink'
+        width: '70%'
       })),
       transition('void => *', animate('400ms ease-in')),
       transition('active => void', animate('400ms ease-out'))
@@ -23,17 +25,85 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
  * Blog main page
  */
 export class AppComponent implements OnInit {
-  title = 'BM';
+  ACTIVESTATE = 'active';
+  INACTIVESTATE = 'inactive';
+  LEFTCOLOR = 'orange';
+  MIDCOLOR = 'yellow';
+  RIGHTCOLOR = 'blue';
+
+
   color: ThemePalette;
-  titleState = '';
+  presentSideNavColor = '';
+  leftColorState = '';
+  midColorState = '';
+  rightColorState = '';
+  leftColorHover = false;
+  midColorHover = false;
+  rightColorHover = false;
 
   ngOnInit() {
-
+    this.presentSideNavColor = this.RIGHTCOLOR;
+    this.changePresentColor(this.presentSideNavColor);
   }
 
-  toggleTitleState() {
-    this.titleState === 'active' ? this.titleState = '' : this.titleState = 'active';
-    // this.screenLevel$.subscribe();
+  changePresentColor(colorName: string) {
+    switch (colorName) {
+      case this.LEFTCOLOR:
+        this.leftColorState = this.ACTIVESTATE;
+        this.midColorState = this.INACTIVESTATE;
+        this.rightColorState = this.INACTIVESTATE;
+        break;
+      case this.MIDCOLOR:
+        this.leftColorState = this.INACTIVESTATE;
+        this.midColorState = this.ACTIVESTATE;
+        this.rightColorState = this.INACTIVESTATE;
+        break;
+      case this.RIGHTCOLOR:
+        this.leftColorState = this.INACTIVESTATE;
+        this.midColorState = this.INACTIVESTATE;
+        this.rightColorState = this.ACTIVESTATE;
+        break;
+      default: this.leftSideNavExpand();
+    }
+    this.presentSideNavColor = colorName;
+  }
+
+  retoreSideNavState() {
+    this.changePresentColor(this.presentSideNavColor);
+    this.leftColorHover = this.midColorHover = this.rightColorHover = false;
+  }
+
+  leftSideNavExpand() {
+    this.leftColorHover = true;
+    setTimeout(() => {
+      if (this.leftColorHover) {
+        this.leftColorState = this.ACTIVESTATE;
+        this.midColorState = this.INACTIVESTATE;
+        this.rightColorState = this.INACTIVESTATE;
+      }
+    }, 100);
+  }
+
+  midSideNavExpand() {
+    this.midColorHover = true;
+    setTimeout(() => {
+      if (this.midColorHover) {
+        this.leftColorState = this.INACTIVESTATE;
+        this.midColorState = this.ACTIVESTATE;
+        this.rightColorState = this.INACTIVESTATE;
+      }
+    }, 100);
+  }
+
+  rightSideNavExpand() {
+    this.rightColorHover = true;
+    setTimeout(() => {
+      if (this.rightColorHover) {
+        this.leftColorState = this.INACTIVESTATE;
+        this.midColorState = this.INACTIVESTATE;
+        this.rightColorState = this.ACTIVESTATE;
+      }
+    }, 100);
   }
 
 }
--- src/assets/md/tech/messy/start up md.md
+++ src/assets/md/tech/messy/start up md.md
@@ -5,3 +5,8 @@
 var tele = '15625507209';
 console.log('call me maybe:' + tele);
 ```
+##Some thing for your mine and your soul
+>xxxxxx
+>xxxxxx
+>XXXXXX
+>Xxxxxxxx
